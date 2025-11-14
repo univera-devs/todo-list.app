d8288d7edbb18829d1c35f9d1870b0fbf0a36f6b
mahdimirzakhani4 <mahdimirzakhani85@gmail.com>
2025-11-14 22:46:47 +0330

    edit backgroud gradient auth page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,8 @@ import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div>
-      <Toaster />
+    <div className='bg-back-950'>
+      <Toaster /> 
       <Routes>
         <Route path="/auth" element={<Auth />}></Route>
       </Routes>
--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -2,14 +2,18 @@ import AuthLayout from '../features/authentication/AuthLayout';
 import Bgsvg from '../features/authentication/Bgsvg';
 
 function Auth() {
-  return (
-    <div className="relative w-full h-screen bg-back-950 overflow-hidden">
-      <Bgsvg />
 
-      <div className="absolute top-0 z-10 w-full h-full flex justify-center items-center">
+  return (
+    <div className="relative flex-col md:flex-row w-full min-h-screen overflow-x-hidden pt-32 md:pt-0 flex items-center justify-end ">
+      <div className='absolute inset-0 overflow-hidden'>
+        <Bgsvg />
+      </div>
+
+      <div className="relative z-10 w-full md:w-1/2 xl:w-2/3 h-full flex justify-center items-center">
         <AuthLayout />
       </div>
     </div>
+
   );
 }
 
