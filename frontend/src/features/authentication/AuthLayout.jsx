468a24f6008a77d3763f8ed28d8bbd107570f3f8
Arvin Jafary <arwinux@gmail.com>
2025-11-14 15:49:44 +0330

    add /auth mobile layout & improve responsive

--- frontend/src/features/authentication/AuthLayout.jsx
+++ frontend/src/features/authentication/AuthLayout.jsx
@@ -15,14 +15,9 @@ function AuthLayout() {
   }, [params, setParams]);
 
   return (
-    <div className="w-full flex-wrap flex justify-center xl:justify-end items-center p-10">
-      <div className="xl:mr-40 max-w-xl w-full flex flex-col gap-y-3 sm:gap-y-12 justify-center items-center">
-        <div className="flex justify-center font-medium rounded-lg text-primary-500 py-2 mb-10 w-full gap-x-3 xl:hidden text-2xl">
-          <p>Hello</p>
-          <p>Welcome</p>
-          <p className=" font-bold">todo app</p>
-        </div>
-        <div className="w-full flex justify-between items-center gap-x-10">
+    <div className="w-full flex-wrap flex justify-center md:justify-end items-center p-10">
+      <div className="xl:mr-40 lg:max-w-lg sm:max-w-sm w-full flex flex-col gap-y-3 sm:gap-y-12 justify-center items-center">
+        <div className="w-full flex justify-between items-center mt-28 gap-x-10">
           <QueryNavLink queryValue="login">Login</QueryNavLink>
           <QueryNavLink queryValue="signup">Sign up</QueryNavLink>
         </div>
